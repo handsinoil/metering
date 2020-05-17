db7f31fecfed28836c716bdc354eec3c527df84c
handsinoil <handsinoil@gmail.com>
2020-05-17 22:07:15 +0400

    немного представления для index.html

--- mysite/meter/views.py
+++ mysite/meter/views.py
@@ -6,10 +6,12 @@ from . models import UnitValue, Rate, Meter, UsersSNT, News
 
 def index(request):
 	listing = News.objects.all()
-	out = ", ".join([q.title for q in listing])
+	users_list = UsersSNT.objects.all()
+
 	template = loader.get_template('meter/index.html')
 	context = {
 		"listing": listing,
+		"users_list": users_list,
 	}
 	#return HttpResponse(template.render(context, request))
 	return render(request, 'meter/index.html', context)
